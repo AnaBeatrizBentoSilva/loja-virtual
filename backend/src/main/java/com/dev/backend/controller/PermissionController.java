1a960d3ae4f1ea5a2f6548e976a99872e6d7d316
AnaBeatrizBentoSilva <anabento3807@gmail.com>
2024-11-25 17:34:41 -0300

    front crud permission

--- backend/src/main/java/com/dev/backend/controller/PermissionController.java
+++ backend/src/main/java/com/dev/backend/controller/PermissionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,21 +25,25 @@ public class PermissionController {
     private PermissionService permissionService;
 
     @GetMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public List<Permission> searchAll(){
         return permissionService.searchAll();
     }
 
     @PostMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Permission insert(@RequestBody Permission permission){
         return permissionService.insert(permission);
     }
 
     @PutMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Permission alter(@RequestBody Permission permission){
         return permissionService.alter(permission);
     }
 
     @DeleteMapping("/{id}")
+    @CrossOrigin("http://localhost:3000")
     public ResponseEntity<Void> delete(@PathVariable("id") Long id){
         permissionService.delete(id);
         return ResponseEntity.ok().build();
